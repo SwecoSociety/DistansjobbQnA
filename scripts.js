46925be16fa2beefe65c52857cb1b058b44fedce
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-03-30 10:38:50 +0200

    console log data

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $.get( "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEnQLr27BEw356nJFgxk6iMdCq53DLXiWEV-LdNZks6wXMNLqp1RlT_n1tIwg1v7avYZDdIkY-Viwq/pub?gid=0&single=true&output=csv", function( data ) {
-	$( ".result" ).html( data );
-	alert( "Load was performed." );
+	//$( ".result" ).html( data );
+	console.log(data)
+	//alert( "Load was performed." );
 });
