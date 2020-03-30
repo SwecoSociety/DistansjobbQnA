3b404b335f2fd5089dc379de530c50c014e7b224
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-03-30 11:24:36 +0200

    br

--- scripts.js
+++ scripts.js
@@ -1,19 +1,11 @@
-function processData(allText) {
+function tsvToArray(allText) {
 	var rows = allText.split(/\r\n|\n/);
 	//var headers = rows[0].split(',');
 	data = []
 	for (var i=1; i<rows.length; i++) {
 		var row = rows[i].split('	');
 		data.push(row)
-		/*if (data.length == headers.length) {
-			var tarr = [];
-			for (var j=0; j<headers.length; j++) {
-				tarr.push(headers[j]+":"+data[j]);
-			}
-			lines.push(tarr);
-		}*/
 	}
-	// alert(lines);
 	return data;
 }
 
@@ -23,25 +15,12 @@ function generateHtmlTable(data) {
 		return null;
 	} else {
 		$.each(data, function( index, row ) {
-			//bind header
-			/*if(index == 0) {
-			html += '<thead>';
-			html += '<tr>';
-			$.each(row, function( index, colData ) {
-				html += '<th>';
-				html += colData;
-				html += '</th>';
-			});
-			html += '</tr>';
-			html += '</thead>';
-			html += '<tbody>';
-			} else {*/
-			
-			html += '<div>';
+			html += '<div class="QnA">';
 			$.each(row, function( index, colData ) {
 				html += '<p>';
 				if(index===0){
-					html += '<b>';
+					
+					html += '<br><b>';
 				}
 				html += colData;
 				if(index===0){
@@ -50,20 +29,18 @@ function generateHtmlTable(data) {
 				html += '</p>';
 			});
 			html += '</div>';
-			//}
 		});
 		html += '</div>';
-		//alert(html);
 		$('body').append(html);
 	}
 }	
 
 $(document).ready(function() {
-	$.get( "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEnQLr27BEw356nJFgxk6iMdCq53DLXiWEV-LdNZks6wXMNLqp1RlT_n1tIwg1v7avYZDdIkY-Viwq/pub?gid=0&single=true&output=tsv", function( csv ) {
+	$.get( "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEnQLr27BEw356nJFgxk6iMdCq53DLXiWEV-LdNZks6wXMNLqp1RlT_n1tIwg1v7avYZDdIkY-Viwq/pub?gid=0&single=true&output=tsv", function( tsv ) {
 		//$( ".result" ).html( data );
-		console.log(csv)
+		console.log(tsv)
 		//alert( "Load was performed." );
-		data = processData(csv)//$.csv.toObjects(csv)
+		data = tsvToArray(tsv)//$.tsv.toObjects(tsv)
 		generateHtmlTable(data)
 		});
 	});
