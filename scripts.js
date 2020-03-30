45da3d3036617e5d810423b4e4ae22ceeb460159
Sven Henrik Karlsson <henrik@macbook-pro-16.lan>
2020-03-30 11:04:44 +0200

    custom simplify

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
 function processData(allText) {
-	var allTextLines = allText.split(/\r\n|\n/);
-	var headers = allTextLines[0].split(',');
-	var lines = [];
-
-	for (var i=1; i<allTextLines.length; i++) {
-		 var data = allTextLines[i].split(',');
-		 if (data.length == headers.length) {
-
-			  var tarr = [];
-			  for (var j=0; j<headers.length; j++) {
-					tarr.push(headers[j]+":"+data[j]);
-			  }
-			  lines.push(tarr);
-		 }
+	var rows = allText.split(/\r\n|\n/);
+	//var headers = rows[0].split(',');
+	data = []
+	for (var i=1; i<rows.length; i++) {
+		var row = rows[i].split(',');
+		data.push(row)
+		/*if (data.length == headers.length) {
+			var tarr = [];
+			for (var j=0; j<headers.length; j++) {
+				tarr.push(headers[j]+":"+data[j]);
+			}
+			lines.push(tarr);
+		}*/
 	}
 	// alert(lines);
-	return lines;
+	return data;
 }
 
 function generateHtmlTable(data) {
